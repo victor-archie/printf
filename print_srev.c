ada7d45830783c38ff028da8830f4d121b8aca8d
machalla1 <adebayohammed2512@gmail.com>
2023-06-30 16:54:57 -0700

    Printf collaboration

---
+++ print_srev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_srev - function will output a str in backward
+ * @args: struct type  va_arg where its appointed  print arguments
+ * Return: the string
+ */
+int print_srev(va_list args)
+{
+
+        char *s = va_arg(args, char*);
+        int p;
+        int q = 0;
+
+        if (s == NULL)
+                s = "(null)";
+        while (s[q] != '\0')
+                q++;
+        for (p = q - 1; p >= 0; p--)
+                _putchar(s[p]);
+        return (q);
