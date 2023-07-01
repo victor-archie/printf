4fe8c6d2f617a0b03ed9675a158108d18b0d411f
victor-archie <victorarchie50@gmial.com>
2023-06-30 17:04:08 -0700

    update

--- print_37.c
+++ print_37.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * printf_37 - handles the % spcifier.
+ * print_37 - handles the % spcifier.
  * Return: 1.
  */
-int printf_37(void)
+int print_37(void)
 {
 	_putchar(37);
 	return (1);
